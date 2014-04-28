d85aa432e2b77fd6e644d1419e2a76aa7d82c94f
Neil Rosenstech <tohellandback@hotmail.fr>
2014-04-28 12:26:28 -0400

    ARMA-5 - History support

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,7 +1,11 @@
 class StatesController < ApplicationController
-  layout false
+  def index
+    @installation = Installation.find(params[:installation_id])
+    @states = @installation.states.order('created_at DESC')
+  end
 
   def diff
     @diff = State.find(params[:id]).diff
+    render layout: false
   end
 end
--- test/controllers/states_controller_test.rb
+++ test/controllers/states_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class StatesControllerTest < ActionController::TestCase
   before do
     @installation = FactoryGirl.create(:installation)
+    @installation_with_states = FactoryGirl.create(:installation_with_states)
     @state1 = FactoryGirl.create(:state)
     @user = FactoryGirl.create(:user)
   end
@@ -19,6 +20,25 @@ class StatesControllerTest < ActionController::TestCase
 
     it 'assigns diff' do
       assigns(:diff).must_equal @state1.diff
+    end
+  end
+
+  describe '#index' do
+    before do
+      sign_in @user
+      get :index, application_id: 12000, installation_id: @installation_with_states.id
+    end
+
+    it 'responds with 200' do
+      response.status.must_equal 200
+    end
+
+    it 'assigns installation' do
+      assigns(:installation).must_equal @installation_with_states
+    end
+
+    it 'assigns states' do
+      assigns(:states).must_equal @installation_with_states.states.order('created_at DESC')
     end
   end
 end
