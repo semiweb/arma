d27349c7e33f262a5da1c9a39d26d2d92dcb8610
Alexis François <alexis.francois.eu@gmail.com>
2014-10-07 13:26:21 -0400

    Replace changelog.content()['description'] by changelog.description

--- app/controllers/code_changelogs_controller.rb
+++ app/controllers/code_changelogs_controller.rb
@@ -21,7 +21,7 @@ class CodeChangelogsController < ApplicationController
 
   def toggle_show_changelog_count
     installation = Installation.find(params[:installation_id])
-    installation.update(show_changelog_count: !installation.show_changelog_count)
+    installation.update_attributes!(show_changelog_count: !installation.show_changelog_count)
     redirect_to application_installation_code_changelogs_path(installation.application_id, installation)
   end
 end
